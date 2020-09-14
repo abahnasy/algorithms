0bf03baf8251ce3b3557370845b0b7f90af16c5c
Ahmed Bahnasy <bahnasy.dev@gmail.com>
2020-09-14 14:09:26 +0200

    adding gitignore file to discard binaries

--- main.cpp
+++ main.cpp
@@ -1,32 +1,14 @@
-#include <stdio.h>
-#include <iostream>
-#include <vector>
 
-#include "Determinant.h"
+#include "PatternMatching.h"
 
 
-using namespace std;
-
-
-
-int main () {
-
-
-
-    vdd mat1 = {{1, 0, 2, -1},
-                     {3, 0, 0, 5},
-                     {2, 1, 4, -3},
-                     {1, 0, 5, 0}
-    };
-
-    vdd mat = {{1,2}, {3,4}};
-
-    cout << get_determinant(mat1, 4);
-
-
-
-
+int main() {
+    string txt =  "ABABDABACDABABCABAB";
+    string pattern = "ABABCABAB";
+    cout << "naive\n";
+    pattern_matching_naive(txt, pattern);
+    cout << "kmp\n";
+    pattern_matching_kmp(txt, pattern);
 
     return 0;
 }
-
